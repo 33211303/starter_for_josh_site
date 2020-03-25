499c71fd17313d5719eb89405275c63ac371913d
33211303 <shania.forth@hotmail.com>
2020-03-25 15:07:35 +0800

    12121

---
+++ footer.php
@@ -0,0 +1,30 @@
+
+        <footer class="container-fluid">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-6 text-center footer-content">
+                        <p> JOSH WHITKIN</p>
+                        <img alt="portrait" src="images/t.jpg">
+                        <img alt="portrait" src="images/r.jpg">
+                    </div>
+                    <div class="col-md-6 text-center footer-content">
+                        <p> TERMS AND CONDITIONS</p>
+                        <img alt="portrait" src="images/l.jpg">
+                        <img alt="portrait" src="images/f.jpg">
+                    </div> 
+
+                </div>
+                <!--row-->
+
+            </div>
+            <!--continer-->
+
+
+        </footer>
+
+
+</body>
+
+</html>
+<?php wp_footer(); ?> 
