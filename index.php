877348ba21215a6b04425f3b783c00d87af27bb5
33211303 <shania.forth@hotmail.com>
2020-04-22 14:47:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ wp_reset_query();?>
                 <h3>LATEST ARTICLES</h3>
             </div>
             <div class="col-md-4 article-cent">
-                <?php $post_id = 6;
+                <?php $post_id = 43;
 $queried_post = get_post($post_id);?>
                 <?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
                 <h4><?php echo $queried_post->post_title; ?></h4>
@@ -45,7 +45,7 @@ $queried_post = get_post($post_id);?>
                 <p class="date">12 December 2020</p>
                 <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
                 </p>
-                <p class="readmo re">CONTINUE READING</p>
+                <p class="readmore">CONTINUE READING</p>
             </div>
         </div><!-- row-->
     </div><!-- container-->
