bd4bd7702ea49f63b9970a453f885939c481c80e
33211303 <shania.forth@hotmail.com>
2020-04-22 12:07:54 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center footer-content">
+                <p> <?php bloginfo('name'); ?></p>
+                <img src=" http://206.189.45.97/~mesh13/wp-content/themes/josh_website_final/images/t.jpg" alt=“tumblr”>
+                <img src=" http://206.189.45.97/~mesh13/wp-content/themes/josh_website_final/images/r.jpg" alt=“rss”>
+            </div>
+            <div class="col-md-6 text-center footer-content">
+                <p>Terms and Conditions</p>
+                <img src=" http://206.189.45.97/~mesh13/wp-content/themes/josh_website_final/images/l.jpg" alt=“linkedin”>
+                <img src=" http://206.189.45.97/~mesh13/wp-content/themes/josh_website_final/images/f.jpg" alt=“facebook”>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php wp_footer(); ?>
