a998119d93ec889099d486acf2a84b62360a1a87
33211303 <shania.forth@hotmail.com>
2020-04-24 01:09:14 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -1,20 +1,15 @@
+<?php
+    /*
+    Template Name: Home page
+    */
+?>
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <section class="container-fluid aboutbg text-center">
     <div class="container">
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=3');//look for posts that have the category of 
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
-            <div><p class="about-text"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>    
- <div class="blocker"></div>
+
+        <!--placeholder for aboutme-->
+
+        <div class="blocker"></div>
     </div><!--  container-->
 </section>
 <section class="container-fluid articlebg ">
@@ -25,29 +20,20 @@ wp_reset_query();?>
                 <h3>LATEST ARTICLES</h3>
             </div>
             <div class="col-md-4 article-cent">
-<?php $post_id = 43;
-$queried_post = get_post($post_id);?>
-                <?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
-                <h4><?php echo $queried_post->post_title; ?></h4>
-                <?php echo $queried_post->post_excerpt; ?>
-                <?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>                       
+
+                <!--placeholder for col1-->
             </div>
-            
+
             <div class="col-md-4 article-cent">
-<?php $post_id = 45;
-$queried_post = get_post($post_id);?>
-                <?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
-                <h4><?php echo $queried_post->post_title; ?></h4>
-                <?php echo $queried_post->post_excerpt; ?>
-                <?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?> 
+
+                <!--placeholder for col1-->
+
+
+
             </div>
             <div class="col-md-4 article-cent">
-<?php $post_id = 47;
-$queried_post = get_post($post_id);?>
-                <?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
-                <h4><?php echo $queried_post->post_title; ?></h4>
-                <?php echo $queried_post->post_excerpt; ?>
-                <?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?> 
+                <!--placeholder for col1-->
+
             </div>
         </div><!-- row-->
     </div><!-- container-->
