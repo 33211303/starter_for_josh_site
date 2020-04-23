15c27e09ce2bc6501c0830b7c5a7d03d077b96a3
33211303 <shania.forth@hotmail.com>
2020-04-23 23:49:05 +0800

    Update page.php

--- page.php
+++ page.php
@@ -11,9 +11,9 @@
 		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
 
 		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-			<?php the_content(); ?>
-		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
+<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
+<?php the_content(); ?>
+<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
         </div><!--postcontet-->
 		
 	</div><!--post class-->
@@ -21,11 +21,11 @@
    
 <?php endwhile; ?>
 	
-	<?php else: ?>
+<?php else: ?>
 	<div class="post">
 		<p>Sorry, no posts found.</p>
 	</div><!--post-->
-	<?php endif; ?>
+<?php endif; ?>
 	</article><!---->
 </section>
            
