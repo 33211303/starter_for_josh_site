50a752d2adcd74474c110078f3547ba4a6e8dec5
33211303 <shania.forth@hotmail.com>
2020-04-23 22:33:19 +0800

    Update cv_template_page.php

--- cv_template_page.php
+++ cv_template_page.php
@@ -13,11 +13,11 @@ Template Name: cv
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
 <div class="post single-page">
-<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
+		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
 
 		<div class="postcontent">
 <?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
-<?php the_content(); ?>
+			<?php the_content(); ?>
 <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
         </div><!--postcontet-->
 		
